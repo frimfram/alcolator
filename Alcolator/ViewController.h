0daeb4f068a56da7c69857a084f57197e9483cda
Jean Ro <jsro88@gmail.com>
2014-11-16 16:38:11 -0800

    Initial Commit

---
+++ Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Jean Ro on 11/16/14.
+//  Copyright (c) 2014 Jean Ro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
